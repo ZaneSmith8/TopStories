44c16758af6db1a926cc5bb43aa354f74bc0e789
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2022-10-03 12:52:21 -0500

    Set up Storyboard

--- TopStories/SourcesViewController.swift
+++ TopStories/SourcesViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SourcesViewController: UITableViewController {
+    // VARIABLES
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // MAIN
+        
+        
     }
 
-
+    // FUNCTIONS
+    
+    
+    
 }
 
